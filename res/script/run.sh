75bd681ef77aaaf2620c7fb66b2613f9f523f3b9
Róbert Ormándi <ormandi.robert@gmail.com>
2011-08-03 11:44:49 +0200

    Taskdef removed from build.xml since an Eclipse issue. Logging is turned on in run.sh

--- res/script/run.sh
+++ res/script/run.sh
@@ -8,7 +8,7 @@ export picture=`basename ${config} .txt`".png";
 export classPath=`ls ../lib/*\.jar | awk '{printf("../%s:",$0)}END{printf("../../bin/gossipLearning.jar")}'`;
 
 mkdir -p results
-cd ../res/config && java -Xmx${memory} -cp ${classPath} peersim.Simulator ${config} >../../bin/results/${result} 2>/dev/null && cd ../../bin
+cd ../res/config && java -Xmx${memory} -cp ${classPath} peersim.Simulator ${config} >../../bin/results/${result} && cd ../../bin
 cat results/${result} | awk 'BEGIN{print "#iter\tmodelSim\terrorSimpe\terrorLocalVoted";}{if ($0 ~ /^[0-9]+/ && length($0) > 0) {if ($0 ~ /ModelObserver$/) {c = $1; m=$2;} else if ($0 ~ /\[0\]/) {e0=$2;} else if ($0 ~ /\[9\]/) {print c "\t" m "\t" e0 "\t" $2;}}}' > results/${restricted_result};
 ../res/script/plot.sh results/${restricted_result} results/${picture};
 
