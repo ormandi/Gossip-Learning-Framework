21a8d086a829849309ca75eed50ed4d2a926935a
Robert Ormandi <ormandi.robert@gmail.com>
2011-10-05 09:53:39 +0200

    Method chunks are added to SimpleLearningProtocol.

--- src/gossipLearning/protocols/SimpleLearningProtocol.java
+++ src/gossipLearning/protocols/SimpleLearningProtocol.java
@@ -6,5 +6,53 @@ import gossipLearning.messages.ModelMessage;
 
 public class SimpleLearningProtocol extends AbstractProtocol {
 
+  @Override
+  public void activeThread() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void passiveThread(ModelMessage message) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public ModelHolder getModelHolder(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setModelHolder(int index, ModelHolder modelHolder) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean add(ModelHolder modelHolder) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public ModelHolder remove(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object clone() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
   
 }
