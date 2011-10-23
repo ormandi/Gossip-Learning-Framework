556bb5edd52d8776030b821a8c09703e3f67f043
csko <cskornel@gmail.com>
2011-10-23 05:02:28 +0200

    run.sh, plot.sh, configs updated to run AdalinePerceptron.

--- res/script/run.sh
+++ res/script/run.sh
@@ -5,12 +5,11 @@ export memory="1G";
 export result=`basename ${config} .txt`"_base_result.txt";
 export restricted_result=`basename ${config} .txt`"_result.txt";
 export picture=`basename ${config} .txt`".png";
-export classPath=`ls ../lib/*\.jar | awk '{printf("../%s:",$0)}END{printf("../../bin/gossipLearning.jar")}'`;
+export classPath=`find -L ../lib/ -name "*.jar"  | awk '{printf("../%s:",$0)}END{printf("../../bin/gossipLearning.jar")}'`;
 
 mkdir -p results
 cd ../res/config && java -Xmx${memory} -cp ${classPath} peersim.Simulator ${config} >../../bin/results/${result} && cd ../../bin
 if [ -e results/${result} ]; then
-  cat results/${result} | awk 'BEGIN{print "#iter\tP2Pegasos\tPegasosMU\tLogReg\tLogRegMU";}{if (NF > 1 && $(NF - 1) ~ /\[0\]$/) {if ($NF ~ /\[0\]/) {printf("%s\t%s", $1, $2);} else {printf("\t%s",$2);} if ($NF ~ /\[3\]/) {print "";}}}' > results/${restricted_result};
+  awk -f ../res/script/result.awk results/${result} > results/${restricted_result};
   ../res/script/plot.sh results/${restricted_result} results/${picture};
 fi
-
