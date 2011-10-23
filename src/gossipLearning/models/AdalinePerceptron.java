780d9eefe7cc19656b6c1e4125d8c7dac0085f77
csko <cskornel@gmail.com>
2011-10-23 04:40:59 +0200

    Adaline Perceptron model.

---
+++ src/gossipLearning/models/AdalinePerceptron.java
@@ -0,0 +1,100 @@
+/**
+ *
+ */
+package gossipLearning.models;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import gossipLearning.interfaces.Model;
+import gossipLearning.utils.Utils;
+
+/**
+ * @author csko
+ * A Model for the Adaline Perceptron.
+ */
+public class AdalinePerceptron implements Model {
+	/** @hidden */
+	protected Map<Integer, Double> w;
+	protected double age;
+
+	/**
+	 * Returns a clone of this object.
+	 * 
+	 * @return the clone of this object
+	 */
+	public Object clone() {
+		AdalinePerceptron tmp = new AdalinePerceptron();
+		tmp.w = new TreeMap<Integer, Double>();
+		for (int k : w.keySet()) {
+			tmp.w.put(k, (double) w.get(k));
+		}
+		tmp.age = age;
+		return tmp;
+	}
+
+	private static final long serialVersionUID = 6040117517300790150L;
+
+	public AdalinePerceptron() {
+		w = new TreeMap<Integer, Double>();
+		age = 0.0;
+	}
+
+	@Override
+	public void init(String prefix) {
+		// w = 0 in sparse representation.
+		age = 0.0;
+	}
+
+	@Override
+	/**
+	 * Performs the update: w_{i+1} = w_i + eta_i * (y - w' * x) * x. 
+	 */
+	public void update(Map<Integer, Double> instance, double label) {
+		// Update age.
+		age++;
+		double rate = 1.0 / age;
+		
+		// Calculate w' * x.
+		int max = Utils.findMaxIdx(w, instance);
+		double s = 0.0;
+	    for (int i = 0; i <= max; i++) {
+	        Double wi = w.get(i);
+	        Double xi = instance.get(i);
+	        if(wi != null && xi != null){
+	        	s += xi * wi;
+	        }
+	    }
+	    // Calculate the update.
+	    for (int i = 0; i <= max; i++) {
+	        Double wi = w.get(i);
+	        Double xi = instance.get(i);
+	        if(xi != null){ // No change if this component is 0.
+		        if(wi == null){
+		        	wi = 0.0;
+		        }
+	        	w.put(i, wi + rate * (label - s) * xi);
+	        }
+	    }
+	}
+
+	@Override
+	/**
+	 * Returns the prediction; signum(w' * x).
+	 */
+	public double predict(Map<Integer, Double> instance) {
+		// Calculate w' * x.
+		int max = Utils.findMaxIdx(w, instance);
+		double s = 0.0;
+	    for (int i = 0; i <= max; i ++) {
+	        Double wi = w.get(i);
+	        Double xi = instance.get(i);
+	        if(wi != null && xi != null){
+	        	s += xi * wi;	        	
+	        }
+	    }
+	    return s >= 0 ? 1.0 : -1.0; 
+	}
+
+}
+
