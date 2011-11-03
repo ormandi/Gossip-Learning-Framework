ae1c39b25f7053f91ce624f8e5f5f75e62777b7e
Istvan Hegedus <ihegedus@inf.u-szeged.hu>
2011-11-03 10:37:26 +0100

    Boosting classes.

---
+++ src/gossipLearning/interfaces/WeakLearner.java
@@ -0,0 +1,53 @@
+package gossipLearning.interfaces;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * This class describes a weak learner for boosting algorithms. The weak learners 
+ * have to candle multiple labeled classification tasks.
+ * @author István Hegedűs
+ *
+ */
+public abstract class WeakLearner implements Model, ProbabilityModel{
+  private static final long serialVersionUID = 1349841351687L;
+  /**
+   * Represents the weight of the current weak learner.
+   */
+  protected double alpha = 1.0;
+  
+  /**
+   * Returns the weight of the current weak learner.
+   * @return weight of the current weak learner
+   */
+  public double getAlpha() {
+    return alpha;
+  }
+  
+  /**
+   * Sets the weight of the current weak learner to the specified value.
+   * @param alpha weight to set
+   */
+  public void setAlpha(double alpha) {
+    this.alpha = alpha;
+  }
+  
+  public abstract Object clone();
+  
+  /**
+   * Calls the weighted version of update function with initial and uniform weights.
+   */
+  public final void update(final Map<Integer, Double> instance, final double label){
+    double[] uniformWeights = new double[getNumberOfClasses()];
+    Arrays.fill(uniformWeights, 1.0 / (double)getNumberOfClasses());
+    update(instance, label, uniformWeights);
+  }
+  
+  /**
+   * Updates the current model respect to the specified parameters.
+   * @param instance training instance
+   * @param label label of training instance
+   * @param weight weights of possible class labels
+   */
+  public abstract void update(final Map<Integer, Double> instance, final double label, final double[] weight);
+}
---
+++ src/gossipLearning/models/LinearRegression.java
@@ -0,0 +1,133 @@
+package gossipLearning.models;
+
+import gossipLearning.interfaces.Mergeable;
+import gossipLearning.interfaces.Model;
+import gossipLearning.interfaces.SimilarityComputable;
+import gossipLearning.utils.Utils;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import peersim.config.Configuration;
+
+/**
+ * This class represents the linear regression learner.
+ * @author István Hegedűs
+ *
+ */
+public class LinearRegression implements Model, Mergeable<LinearRegression>, SimilarityComputable<LinearRegression> {
+  private static final long serialVersionUID = -1468280308189482885L;
+  
+  /**
+   * The learning parameter is 0.0001 by default.
+   */
+  protected static final String PAR_LAMBDA = "lambda";
+  protected double lambda = 0.0001;
+  
+  /** @hidden */
+  private Map<Integer, Double> w;
+  private double age;
+  
+  /**
+   * Creates a default model with age=0 and the regression hyperplane is the 0 vector.
+   */
+  public LinearRegression(){
+    w = new TreeMap<Integer, Double>();
+    age = 0.0;
+  }
+  
+  private LinearRegression(Map<Integer, Double> w, double age, double lambda){
+    this.w = new TreeMap<Integer, Double>();
+    for (int k : w.keySet()){
+      this.w.put(k, (double)w.get(k));
+    }
+    this.age = age;
+    this.lambda = lambda;
+  }
+  
+  public Object clone(){
+    return new LinearRegression(w, age, lambda);
+  }
+
+  /**
+   * Initialize the age=0 and the separating hyperplane=0 vector.
+   */
+  @Override
+  public void init(String prefix) {
+    w = new TreeMap<Integer, Double>();
+    age = 0.0;
+    lambda = Configuration.getDouble(prefix + "." + PAR_LAMBDA, 0.0001);
+  }
+
+  @Override
+  public void update(Map<Integer, Double> instance, double label) {
+    double err = label - Utils.innerProduct(w, instance);
+    age ++;
+    double nu = 1.0 / (lambda * age);
+    int max = Utils.findMaxIdx(w, instance);
+    for (int i = 0; i <= max; i ++) {
+      Double wOldCompD = w.get(i);
+      Double xCompD = instance.get(i);
+      // using w0 as bias
+      if (i == 0) {
+        xCompD = 1.0;
+      }
+      if (wOldCompD != null || xCompD != null) {
+        double wOldComp = (wOldCompD == null) ? 0.0 : wOldCompD.doubleValue();
+        double xComp = (xCompD == null) ? 0.0 : xCompD.doubleValue();
+        w.put(i, (1.0 - 1.0 / age) * wOldComp - nu * err * xComp);
+      }
+    }
+  }
+
+  /**
+   * In case of linear regression the prediction is w*x + b.
+   */
+  @Override
+  public double predict(Map<Integer, Double> instance) {
+    double b = 0.0;
+    if (w.containsKey(0)) {
+      b = w.get(0);
+    }
+    return Utils.innerProduct(w, instance) + b;
+  }
+
+  @Override
+  public double computeSimilarity(LinearRegression model) {
+    return Utils.computeSimilarity(w, model.w);
+  }
+
+  @Override
+  public LinearRegression merge(LinearRegression model) {
+    Map<Integer, Double> mergedw = new TreeMap<Integer, Double>();
+    double age = Math.round((this.age + model.age) / 2.0);
+    double value;
+    for (int i : w.keySet()){
+      value = w.get(i);
+      if (model.w.containsKey(i)){
+        value += model.w.get(i);
+      }
+      value /= 2.0;
+      mergedw.put(i, value);
+    }
+    for (int i : model.w.keySet()){
+      if (!w.containsKey(i)){
+        mergedw.put(i, model.w.get(i) / 2.0);
+      }
+    }
+    return new LinearRegression(mergedw, age, lambda);
+  }
+
+  @Override
+  public int getNumberOfClasses() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public void setNumberOfClasses(int numberOfClasses) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
