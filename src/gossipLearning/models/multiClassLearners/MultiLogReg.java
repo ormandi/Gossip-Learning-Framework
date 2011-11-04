78f739ef681700bf913812b82b09e66f1a9849de
Istvan Hegedus <ihegedus@inf.u-szeged.hu>
2011-11-04 14:24:09 +0100

    MultiLogReg is final.

--- src/gossipLearning/models/multiClassLearners/MultiLogReg.java
+++ src/gossipLearning/models/multiClassLearners/MultiLogReg.java
@@ -9,13 +9,26 @@ import gossipLearning.interfaces.Model;
 import gossipLearning.interfaces.ProbabilityModel;
 import gossipLearning.utils.Utils;
 
-public class MultiLogReg implements Model, ProbabilityModel {
+/**
+ * This class represents the multi-class logistic regression classifier. 
+ * Uses Map<Integer,Double> collections to represent hyperplanes and the 
+ * -1th element of the collections represents the bias. 
+ * This code is based on the Machine Learning book from Tom M. Mitchell.
+ * <br/><br/>
+ * Required configuration parameters:<br/>
+ * <ul>
+ * <li>MLR.lambda - learning rate</li>
+ * </ul>
+ * @author István Hegedűs
+ *
+ */
+public class MultiLogReg extends ProbabilityModel {
   private static final long serialVersionUID = -3918448404565337980L;
   
   /**
    * The learning parameter is 0.0001 by default.
    */
-  private static final String PAR_LAMBDA = "lambda";
+  private static final String PAR_LAMBDA = "MLR.lambda";
   private double lambda = 0.0001;
   
   private Map<Integer, Double>[] w;
@@ -24,7 +37,8 @@ public class MultiLogReg implements Model, ProbabilityModel {
 
   /**
    * Constructs a default multi-class logistic regression. <br/>
-   * NOTE: 
+   * NOTE: It works only after calling init(String prefix) and 
+   * setNumberOfClasses(int numberOfClasses) functions.
    */
   public MultiLogReg() {
     w = null;
@@ -66,16 +80,21 @@ public class MultiLogReg implements Model, ProbabilityModel {
     double[] distribution = new double[numberOfClasses];
     double sum = 0.0;
     for (int i = 0; i < numberOfClasses -1; i++) {
-      double bias = 0.0;
+      /*double bias = 0.0;
       if (w[i].containsKey(-1)) {
         bias = w[i].get(-1);
       }
-      distribution[i] = Math.exp(bias + Utils.innerProduct(w[i], instance));
+      distribution[i] = Math.exp(bias + Utils.innerProduct(Utils.normalize(w[i]), instance));
+      */
+      distribution[i] = Math.exp(Utils.innerProduct(Utils.normalize(w[i]), instance));
       sum += distribution[i];
     }
     distribution[numberOfClasses - 1] = 1.0;
     for (int i = 0; i < numberOfClasses; i++) {
       distribution[i] /= 1.0 + sum;
+      if (Double.isNaN(distribution[i])) {
+        System.out.println(w[i] + "\n" + instance + "\t" + sum);
+      }
     }
     return distribution;
   }
@@ -97,34 +116,20 @@ public class MultiLogReg implements Model, ProbabilityModel {
       double cDelta = (label == j) ? 1.0 : 0.0;
       double err = cDelta - distribution[j];
       
-      for (int i = -1; i <= max; i ++) {
+      for (int i = 0; i <= max; i ++) {
         Double wOldCompD = w[j].get(i);
         Double xCompD = instance.get(i);
         // using w0 as bias
-        if (i == -1) {
+        /*if (i == -1) {
           xCompD = 1.0;
-        }
+        }*/
         if (wOldCompD != null || xCompD != null) {
           double wOldComp = (wOldCompD == null) ? 0.0 : wOldCompD.doubleValue();
           double xComp = (xCompD == null) ? 0.0 : xCompD.doubleValue();
-          w[i].put(i, (1.0 - nu * lambda) * wOldComp - nu * err * xComp);
+          w[j].put(i, (1.0 - nu * lambda) * wOldComp + nu * err * xComp);
         }
       }
     }
-  }
-
-  @Override
-  public double predict(Map<Integer, Double> instance) {
-    double[] distribution = distributionForInstance(instance);
-    int maxIndex = -1;
-    double maxValue = Double.NEGATIVE_INFINITY;
-    for (int i = 0; i < numberOfClasses; i++) {
-      if (distribution[i] > maxValue) {
-        maxValue = distribution[i];
-        maxIndex = i;
-      }
-    }
-    return maxIndex;
   }
 
   @Override
