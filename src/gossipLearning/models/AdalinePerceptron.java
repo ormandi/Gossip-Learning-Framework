e018eb6cd0915109bebbc505fef9187bf65c7666
Istvan Hegedus <ihegedus@inf.u-szeged.hu>
2012-01-04 13:50:41 +0100

    AdalinePerceptron: some note was added.

--- src/gossipLearning/models/AdalinePerceptron.java
+++ src/gossipLearning/models/AdalinePerceptron.java
@@ -12,6 +12,8 @@ import gossipLearning.utils.Utils;
 /**
  * @author csko
  * A Model for the Adaline Perceptron.
+ * @note This implementation provides teaching reasons. Another version of the Adaline 
+ * perceptron can be found in the weakLearners package.
  */
 public class AdalinePerceptron implements Model {
 	/** @hidden */
