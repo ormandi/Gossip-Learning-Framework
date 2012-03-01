e68271a7e1237592c6e50d0ea0d41c38c160f97e
Robert Ormandi <ormandi.robert@gmail.com>
2012-03-01 14:41:57 +0100

    UCB config

--- res/script/run.sh
+++ res/script/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export config=`basename $1`;
-export memory="10G";
+export memory="1G";
 export result=`basename ${config} .txt`"_base_result.txt";
 export restricted_result=`basename ${config} .txt`"_result.txt";
 export picture=`basename ${config} .txt`".png";
@@ -10,6 +10,6 @@ export classPath=`find -L ../lib/ -name "*.jar"  | awk '{printf("%s:",$0)}END{pr
 mkdir -p results
 java -Xmx${memory} -cp ${classPath} peersim.Simulator $1 >results/${result}
 if [ -e results/${result} ]; then
-  awk -f ../res/script/result.awk results/${result} > results/${restricted_result};
-  ../res/script/plot.sh results/${restricted_result} results/${picture};
+  #awk -f ../res/script/result.awk results/${result} > results/${restricted_result};
+  #../res/script/plot.sh results/${restricted_result} results/${picture};
 fi
