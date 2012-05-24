363e1a1eba4cc495bab6173036549ff2a3a2fc83
Robert Ormandi <ormandi.robert@gmail.com>
2012-05-24 10:41:00 +0200

    SimpleBP was extended with merge capability

--- src/gossipLearning/protocols/SimpleBanditProtocol2SentModels.java
+++ src/gossipLearning/protocols/SimpleBanditProtocol2SentModels.java
@@ -1,5 +1,6 @@
 package gossipLearning.protocols;
 
+import gossipLearning.interfaces.Mergeable;
 import gossipLearning.interfaces.Model;
 import gossipLearning.interfaces.ModelHolder;
 import gossipLearning.messages.ModelMessage;
@@ -8,6 +9,11 @@ import gossipLearning.models.bandits.BanditModel;
 
 public class SimpleBanditProtocol2SentModels extends SimpleLearningProtocol {
   private final String prefix;
+  
+  /** @hidden */
+  protected Mergeable<? extends BanditModel> lastSeenMergeableModel;
+  
+  
   public SimpleBanditProtocol2SentModels(String prefix) {
     super(prefix);
     this.prefix = prefix;
@@ -33,6 +39,7 @@ public class SimpleBanditProtocol2SentModels extends SimpleLearningProtocol {
     }
   }
   
+  @SuppressWarnings({ "unchecked", "rawtypes" })
   @Override
   public void passiveThread(ModelMessage message) {
     for (int incommingModelID = 0; message != null && incommingModelID < message.size(); incommingModelID ++) {
@@ -42,6 +49,15 @@ public class SimpleBanditProtocol2SentModels extends SimpleLearningProtocol {
       // this protocol can work with Bandit models
       if (! (model instanceof BanditModel)) {
         throw new RuntimeException("BanditProtocol can work only with BanditModels!");
+      }
+      
+      // store the model as last seen
+      if (model instanceof Mergeable) {
+        Mergeable<?> modelForMerge = lastSeenMergeableModel;
+        lastSeenMergeableModel = (Mergeable<? extends BanditModel>) model.clone();
+        if (modelForMerge != null) {
+          model = ((Mergeable) model).merge((BanditModel) modelForMerge);
+        }
       }
       
       // update 
