1430e42f56ddf6693a998dc59da670b9baa6510a
Robert Ormandi <ormandi.robert@gmail.com>
2012-07-19 15:49:15 +0200

    RecSys package first version is added which is untested

---
+++ src/gossipLearning/models/recSys/AbstractRecSysModel.java
@@ -0,0 +1,295 @@
+package gossipLearning.models.recSys;
+
+import gossipLearning.interfaces.Model;
+import gossipLearning.interfaces.VectorEntry;
+import gossipLearning.utils.SparseVector;
+
+import java.util.Vector;
+
+import peersim.config.Configuration;
+import peersim.core.CommonState;
+import peersim.core.Network;
+import peersim.core.Node;
+
+/**
+ * 
+ * @author Róbert Ormándi
+ */
+public abstract class AbstractRecSysModel implements RecSysModel {
+  protected static final long serialVersionUID = 8437206766184831672L;
+  
+  /**
+   * This parameter defines how often perform model update steps relative to the 
+   * item set updates. The default value is 10 which means the set update is 10 times
+   * more frequent than the model updates.
+   */
+  protected static final String PAR_MODEL_UPDATE_FREQUENCY = "recsys.modelUpdateFrequency";
+  protected static final int DEFAULT_MODEL_UPDATE_FREQUENCY = 10;
+  
+  /**
+   * It defines the number of counters applied in the Bloom Filters that represents the
+   * item sets. 
+   */
+  protected static final String PAR_NUMBER_OF_COUNTERS = "recsys.numberOfCounters";
+  protected static final int DEFAULT_NUMBER_OF_COUNTERS = 30;
+  
+  /**
+   * It defines the number of hash functions used in the Bloom Filters that describes the item
+   * related features.
+   */
+  protected static final String PAR_NUMBER_OF_HASH_FUNCTIONS = "recsys.numberOfHashFunctions";
+  protected static final int DEFAULT_NUMBER_OF_HASH_FUNCTIONS = 4;
+  
+  /**
+   * We use a concrete feature set for model based learning in which there is a fixed number of feature. 
+   * Since the number of features can be considered to constant.
+   */
+  protected static final int NUMBER_OF_FEATURES = 11;
+  
+  /**
+   * It defines the number of clusters. The default value of this parameter is zero meaning no clustering performed.
+   */
+  protected static final String PAR_NUMBER_OF_CLUSTERS = "recsys.clusters";
+  protected static final int DEFAULT_NUMBER_OF_CLUSTERS = 0;  // it means no clustering performed
+  
+  /**
+   * Defines how many steps should be taken by the clusterer.
+   */
+  protected static final String PAR_NUMBER_OF_CLUSTERING_STEPS = "recsys.clusteringSteps";
+  protected static final int DEFAULT_NUMBER_OF_CLUSTERING_STEPS = 200;
+  
+  protected int numberOfItems = 0;
+  protected int numberOfRatings = 0;
+  protected int modelUpdateFrequency = DEFAULT_MODEL_UPDATE_FREQUENCY;
+  protected int numberOfClusters = DEFAULT_NUMBER_OF_CLUSTERS;
+  protected int numberOfClusteringSteps = DEFAULT_NUMBER_OF_CLUSTERING_STEPS;
+  protected Node node = null;
+  protected int numberOfCounters = DEFAULT_NUMBER_OF_COUNTERS;
+  protected int numberOfHashFunctions = DEFAULT_NUMBER_OF_HASH_FUNCTIONS;
+  
+  // set representation
+  protected ItemFrequencies itemFreqs;
+  
+  @Override
+  public Object clone() {
+    throw new RuntimeException("Cloning the " + getClass().getCanonicalName() + " is not possible since it is abstract!");
+  }
+  
+  @Override
+  public void init(String prefix) {
+    modelUpdateFrequency = Configuration.getInt(prefix + "." + PAR_MODEL_UPDATE_FREQUENCY, DEFAULT_MODEL_UPDATE_FREQUENCY);
+    numberOfClusters = Configuration.getInt(prefix + "." + PAR_NUMBER_OF_CLUSTERS, DEFAULT_NUMBER_OF_CLUSTERS);
+    numberOfClusteringSteps = Configuration.getInt(prefix + "." + PAR_NUMBER_OF_CLUSTERING_STEPS, DEFAULT_NUMBER_OF_CLUSTERING_STEPS);
+    numberOfCounters = Configuration.getInt(prefix + "." + PAR_NUMBER_OF_COUNTERS, DEFAULT_NUMBER_OF_COUNTERS);
+    numberOfHashFunctions = Configuration.getInt(prefix + "." + PAR_NUMBER_OF_HASH_FUNCTIONS, DEFAULT_NUMBER_OF_HASH_FUNCTIONS);
+  }
+
+  /**
+   * It receives a special type of training example which can be described as follows:<br/>
+   * The instance contains the rates of a user in sparse format i.e. the keys of the instance
+   * are the item IDs and the values are the ratings. Where the rating equals to 0 it means
+   * no rating is given.<br/>
+   * The label refers to the user who assigned the given ratings to the items i.e. the label contains
+   * the user ID.
+   */
+  @Override
+  public void update(SparseVector instance, double label) {
+    // store the node to which the current model arrived
+    final int userID = (int) label; 
+    node = Network.get(userID);
+    
+    // compute the averaged rating of the user
+    double userAvgRating = 0.0, userNumberOfRatings = 0.0;
+    for (VectorEntry itemRating : instance) {
+      userAvgRating += itemRating.value;
+      userNumberOfRatings ++;
+    }
+    userAvgRating = (userNumberOfRatings > 0.0) ? userAvgRating / userNumberOfRatings : (numberOfRatings + 1.0) / 2.0; // when no rating for a user, use the mean of rating range
+    
+    // perform item set update
+    for (VectorEntry itemRating : instance) {
+      int itemID = itemRating.index;
+      double rating = itemRating.value;
+      itemFreqs.add(itemID, rating, userAvgRating);
+    }
+    
+    // check whether model update is needed or not
+    if (CommonState.getTime() % modelUpdateFrequency == 0) {
+      // perform feature extraction for model update
+      double[] featureVectorA = new double[NUMBER_OF_FEATURES];
+      featureVectorA[0] = userAvgRating;
+      featureVectorA[1] = userNumberOfRatings;
+      double likeSize = 0.0, disLikeSize = 0.0;
+      double numberOfAllUsers = 10000.0;          // FIXME: correct normalization constant
+      
+      // perform item clustering
+      Model clusterer = null;
+      if (numberOfClusters > 0) {
+        // create database for clustering
+        Vector<SparseVector> clusteringDB = new Vector<SparseVector>();
+        for (VectorEntry itemRating : instance) {
+          int itemID = itemRating.index;
+          double[] clusteringDBVec = new double[3];
+          
+          clusteringDBVec[0] = itemFreqs.getAverageRating(itemID);
+          clusteringDBVec[1] = (double)itemFreqs.getNumberOfUsers(itemID) / numberOfAllUsers;
+          clusteringDBVec[2] = itemFreqs.getNumberOfLikeableUsers(itemID);
+          
+          clusteringDB.add(new SparseVector(clusteringDBVec));
+        }
+        
+        // perform clustering
+        clusterer = new KMeans(numberOfClusters);
+        for (int step = 1; step <= numberOfClusteringSteps; step ++) {
+          clusterer.update(clusteringDB.get(clusteringDB.size() % step), 0.0);
+        }
+      }
+      
+      
+      // extract features
+      for (VectorEntry itemRating : instance) {
+        int itemID = itemRating.index;
+        double rating = itemRating.value; // classLabel
+        
+        double itemPop = (double)itemFreqs.getNumberOfUsers(itemID) / numberOfAllUsers;
+        double itemAvg = itemFreqs.getAverageRating(itemID);
+        
+        // general
+        featureVectorA[2] += itemPop;
+        featureVectorA[3] += itemAvg;
+        
+        // like
+        if (rating > userAvgRating) {
+          featureVectorA[4] += itemPop;
+          featureVectorA[5] += itemAvg;
+          likeSize ++;
+        }
+        
+        // dislike
+        if (rating < userAvgRating) {
+          featureVectorA[6] += itemPop;
+          featureVectorA[7] += itemAvg;
+          disLikeSize ++;
+        }
+      }
+      
+      // normalize features
+      featureVectorA[2] = (instance.size() > 0) ? featureVectorA[2] / (double)instance.size() : 0.0;
+      featureVectorA[3] = (instance.size() > 0) ? featureVectorA[3] / (double)instance.size() : 0.0;
+      featureVectorA[4] = (likeSize > 0.0) ? featureVectorA[4] / likeSize : 0.0;
+      featureVectorA[5] = (likeSize > 0.0) ? featureVectorA[5] / likeSize : 0.0; 
+      featureVectorA[6] = (disLikeSize > 0.0) ? featureVectorA[6] / disLikeSize : 0.0;
+      featureVectorA[7] = (disLikeSize > 0.0) ? featureVectorA[7] / disLikeSize : 0.0;
+      
+      // continue feature extraction on item specific features and update models
+      for (VectorEntry itemRating : instance) {
+        int itemID = itemRating.index;
+        double rating = itemRating.value; // classLabel
+        
+        // perform 
+        featureVectorA[8] = itemFreqs.getAverageRating(itemID);
+        featureVectorA[9] = (double)itemFreqs.getNumberOfUsers(itemID) / numberOfAllUsers;
+        featureVectorA[10] = itemFreqs.getNumberOfLikeableUsers(itemID);
+        
+        // get the cluster ID if necessary
+        int clusterID = itemID;
+        if (numberOfClusters > 0) {
+          SparseVector clusteringInstance = new SparseVector(3);
+          clusteringInstance.put(0, featureVectorA[8]);
+          clusteringInstance.put(1, featureVectorA[9]);
+          clusteringInstance.put(2, featureVectorA[10]);
+          clusterID = (int) clusterer.predict(clusteringInstance);
+        }
+        
+        // create final feature vector (the corresponding class label is in variable rating)
+        SparseVector featureVector = new SparseVector(featureVectorA);
+        
+        // update model
+        getModel(clusterID, rating).update(featureVector, rating);
+      }
+    }
+  }
+
+  /**
+   * Returns the number of ratings.
+   * 
+   * @return number of ratings
+   */
+  @Override
+  public int getNumberOfClasses() {
+    return numberOfRatings;
+  }
+  
+  /**
+   * Sets the number of ratings. Since - in some cases - the model structure depends on
+   * the number of ratings (see RBM recSys) this call reinitialize the model structure.
+   * Moreover the item set structure also depends on the number of ratings the set structure
+   * is reinitialized as well.
+   * 
+   * @param numberOfClasses new number of ratings 
+   */
+  @Override
+  public void setNumberOfClasses(int numberOfClasses) {
+    if (numberOfClasses > 0) {
+      numberOfRatings = numberOfClasses;
+      
+      // reinitialize set structure
+      itemFreqs = new ItemFrequencies(numberOfRatings, numberOfCounters, numberOfHashFunctions);
+      
+      // reinitialize model structure
+      initializeModels(numberOfClusters, numberOfRatings);
+    } else {
+      throw new RuntimeException("The number of ratings shouldn't be negative of zero!");
+    }
+  }
+  
+  /**
+   * Returns the number of items.
+   * 
+   * @return number of items
+   */
+  @Override
+  public int getNumberOfItems() {
+    return numberOfItems;
+  }
+  
+  /**
+   * Sets the number of items.
+   * 
+   * @param numberOfClasses new number of ratings 
+   */
+  @Override
+  public void setNumberOfItems(int items) {
+    numberOfItems = items;
+  }
+  
+  /**
+   * Returns a reference to the node which currently stores this model. The node update is
+   * performed in the updateModel method which is called always when a model arrives to a new node.
+   * 
+   * @return reference to the node which holds this model
+   */
+  @Override
+  public Node getNode() {
+    return node;
+  }
+  
+  // --------------- Abstract Methods ---------------
+  
+  /**
+   * Initializes the model structure.
+   */
+  protected abstract void initializeModels(int numberOfClusters, int numberOfRatings);
+  
+  /**
+   * Returns the model that is responsible for handling the given clusterID and rating value.<br/>
+   * In some implementation the model selection depends on either of the parameters. In these cases
+   * the value of the non-dependent parameter can be arbitrary.
+   * 
+   * @param clusterID itemID of the model (only in case of item based models)
+   * @param rating rating of the model (only in case of rate based models)
+   * @return responsible model
+   */
+  protected abstract Model getModel(int clusterID, double rating);
+
+}
---
+++ src/gossipLearning/models/recSys/ItemFrequencies.java
@@ -0,0 +1,158 @@
+package gossipLearning.models.recSys;
+
+import gossipLearning.utils.MultiBloomFilter;
+
+import java.io.Serializable;
+
+/**
+ * Space efficient item frequency and likeability estimator class based on counting Bloom Filter implementation.
+ *  
+ * @author Róbert Ormándi
+ */
+public class ItemFrequencies implements Serializable {
+  private static final long serialVersionUID = 1348239L;
+  private MultiBloomFilter[] ratingSet;
+  private MultiBloomFilter[] likeabilitySet;
+  
+  private int m;
+  private int k;
+  
+  /**
+   * Creates a new instance.
+   * 
+   * @param numberOfRatings defines how many ratings are possible in the recommender task
+   * @param m number of counters applied by the wrapped bloom filters
+   * @param k number of hash functions applied by the inner bloom filters
+   */
+  public ItemFrequencies(int numberOfRatings, int m, int k) {
+    this.m = m;
+    this.k = k;
+    ratingSet = new MultiBloomFilter[numberOfRatings];
+    likeabilitySet = new MultiBloomFilter[numberOfRatings];
+  }
+  
+  /**
+   * Copy constructor which produce a deep copy of the set using the clone methods of the bloom filters.
+   * 
+   * @param other the instance that should be copied
+   */
+  public ItemFrequencies(ItemFrequencies other) {
+    // copy simple fields
+    m = other.m;
+    k = other.k;
+    
+    // deep copy of rating set
+    ratingSet = (other.ratingSet == null) ? null : new MultiBloomFilter[other.ratingSet.length];
+    for (int i = 0; other.ratingSet != null && i < other.ratingSet.length; i ++) {
+      ratingSet[i] = (MultiBloomFilter) other.ratingSet[i].clone();
+    }
+    
+    // deep copy of likeability set
+    likeabilitySet = (other.likeabilitySet == null) ? null : new MultiBloomFilter[other.ratingSet.length];
+    for (int i = 0; other.likeabilitySet != null && i < other.likeabilitySet.length; i ++) {
+      likeabilitySet[i] = (MultiBloomFilter) other.likeabilitySet[i].clone();
+    }
+  }
+  
+  /**
+   * It produces a deep copy applying the copy constructor.
+   */
+  public Object clone() {
+    return new ItemFrequencies(this);
+  }
+  
+  /**
+   * Add an item to the data structure
+   * 
+   * @param itemID ID of the item which is added
+   * @param rating rating of the item given by a user
+   * @param userAvgRating average rating value of the user who adds the item (this is necessary for computing likeability)
+   */
+  public void add(int itemID, double rating, double userAvgRating) {
+    // get rating ID
+    int rateID = (int)rating - 1;
+    
+    // add to rate set
+    if (0 <= rateID && rateID < ratingSet.length) {
+      // initialize bloom filter if necessary
+      if (ratingSet[rateID] == null) {
+        ratingSet[rateID] = new MultiBloomFilter(m, k);
+      }
+      // add item
+      ratingSet[rateID].add(itemID);
+    } else {
+      throw new RuntimeException("Rating " + rating + " out of range in item set with length " + ratingSet.length);
+    }
+    
+    // add to likeability
+    if (0 <= rateID && rateID < ratingSet.length) {
+      // initialize if necessary
+      if (likeabilitySet[rateID] == null) {
+        likeabilitySet[rateID] = new MultiBloomFilter(m, k);
+      }
+      // add item if necessary
+      if (rating > userAvgRating) {
+        likeabilitySet[rateID].add(itemID);
+      }
+    } else {
+      throw new RuntimeException("Rating " + rating + " out of range in likeability set with length " + likeabilitySet.length);
+    }
+  }
+  
+  /**
+   * Counts the number of users who rated the give item.
+   * 
+   * @param itemID ID of the item
+   * @return the number of users who rated the item
+   */
+  public int getNumberOfUsers(int itemID) {
+    return sumOccurrences(ratingSet, itemID);
+  }
+  
+  /**
+   * Computes the averaged rating of the users who rated the given item.
+   * 
+   * @param itemID ID of the item
+   * @return averaged user rating
+   */
+  public double getAverageRating(int itemID) {
+    double avg = 0.0;
+    double sum = 0.0;
+    for (int i = 0; i < ratingSet.length; i ++) {
+      if (ratingSet[i] != null) {
+        avg += ratingSet[i].contains(itemID) * (double)i;
+        sum += ratingSet[i].contains(itemID);
+      }
+    }
+    return (sum > 0.0) ? avg/sum : 0.0;
+  }
+  
+  /**
+   * Returns the number of users who liked the give item.<br/>
+   * Precisely, it computes the number of users who rated the given item and
+   * added higher rate than their averaged rate.
+   * 
+   * @param itemID ID of the item
+   * @return number of users who liked the item
+   */
+  public int getNumberOfLikeableUsers(int itemID) {
+    return sumOccurrences(likeabilitySet, itemID);
+  }
+  
+  /**
+   * Returns the sum of occurrences of the given item in the sets.
+   *  
+   * @param sets array of sets in which we seek for occurrences
+   * @param itemID item ID
+   * @return sum of occurrences
+   */
+  private int sumOccurrences(MultiBloomFilter[] sets, int itemID) {
+    int sum = 0;
+    for (int i = 0; i < sets.length; i ++) {
+      if (sets[i] != null) {
+        sum += sets[i].contains(itemID);
+      }
+    }
+    return sum;
+  }
+}
