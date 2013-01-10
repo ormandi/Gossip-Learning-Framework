8a125034ccfa9c06028ee94074d14708c6318444
arppy <arphead@gmail.com>
2013-01-10 14:02:24 +0100

    NMI was bugfexed and tested. Purity was also tested.

--- src/gossipLearning/controls/observers/errorComputation/ConfusionMtxFunction.java
+++ src/gossipLearning/controls/observers/errorComputation/ConfusionMtxFunction.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * This abstract class supports the confusion matrix based measurements 
+ * This abstract class supports the confusion matrix based measurements
  * by computing the matrix through the computeError function calls.
  * @note At the end of the postProcess function the matrix should be clear!
  * @author István Hegedűs
@@ -14,56 +14,65 @@ import java.util.TreeMap;
  */
 public abstract class ConfusionMtxFunction implements ErrorFunction {
 
-  /**
-   * Represents the confusion matrix.
-   */
-  protected Map<Integer, SparseVector> confusionMatrix;
-  
-  @Override
-  public final double computeError(double expected, double predicted) {
-    if (expected != (int)expected || predicted != (int)predicted) {
-      throw new RuntimeException("The expected and the predicted labels should be integer values: " + expected + " " + predicted);
-    }
-    if (confusionMatrix == null) {
-      confusionMatrix = new TreeMap<Integer, SparseVector>();
-    }
-    SparseVector row = confusionMatrix.get((int)expected);
-    if (row == null) {
-      row = new SparseVector();
-      confusionMatrix.put((int)expected, row);
-    }
-    double value = row.get((int)predicted);
-    value ++;
-    row.put((int)predicted, value);
-    return 0.0;
-  }
-  
-  public String toString() {
-    StringBuffer sb = new StringBuffer();
-    int numClasses = 0;
-    sb.append("e\\p");
-    for (int i : confusionMatrix.keySet()) {
-      sb.append('\t');
-      sb.append(i);
-      if (numClasses <= i) {
-        numClasses = i + 1;
-      }
-    }
-    sb.append('\n');
-    for (int i = 0; i < numClasses; i++) {
-      SparseVector v = confusionMatrix.get(i);
-      sb.append(i);
-      for (int j = 0; j < numClasses; j++) {
-        sb.append('\t');
-        if (v == null || v.get(j) == 0.0) {
-          sb.append(0);
-        } else {
-          sb.append((int)v.get(j));
-        }
-      }
-      sb.append('\n');
-    }
-    return sb.toString();
-  }
+	/**
+	 * Represents the confusion matrix.
+	 */
+	protected Map<Integer, SparseVector> confusionMatrix;
+	
+	
+	/***
+	 * Test use only!!! 
+	 * @author Arpad Berta
+	 */
+	public void setConfusionMatrix(Map<Integer, SparseVector> confusionMatrix) {
+		this.confusionMatrix = confusionMatrix;
+	}
+
+	@Override
+	public final double computeError(double expected, double predicted) {
+		if (expected != (int)expected || predicted != (int)predicted) {
+			throw new RuntimeException("The expected and the predicted labels should be integer values: " + expected + " " + predicted);
+		}
+		if (confusionMatrix == null) {
+			confusionMatrix = new TreeMap<Integer, SparseVector>();
+		}
+		SparseVector row = confusionMatrix.get((int)expected);
+		if (row == null) {
+			row = new SparseVector();
+			confusionMatrix.put((int)expected, row);
+		}
+		double value = row.get((int)predicted);
+		value ++;
+		row.put((int)predicted, value);
+		return 0.0;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		int numClasses = 0;
+		sb.append("e\\p");
+		for (int i : confusionMatrix.keySet()) {
+			sb.append('\t');
+			sb.append(i);
+			if (numClasses <= i) {
+				numClasses = i + 1;
+			}
+		}
+		sb.append('\n');
+		for (int i = 0; i < numClasses; i++) {
+			SparseVector v = confusionMatrix.get(i);
+			sb.append(i);
+			for (int j = 0; j < numClasses; j++) {
+				sb.append('\t');
+				if (v == null || v.get(j) == 0.0) {
+					sb.append(0);
+				} else {
+					sb.append((int)v.get(j));
+				}
+			}
+			sb.append('\n');
+		}
+		return sb.toString();
+	}
 
 }
--- src/gossipLearning/controls/observers/errorComputation/NMI.java
+++ src/gossipLearning/controls/observers/errorComputation/NMI.java
@@ -4,54 +4,69 @@ import gossipLearning.interfaces.VectorEntry;
 import gossipLearning.utils.SparseVector;
 
 /**
- * This class can be used for computing the NMI (normalized mutual 
+ * This class can be used for computing the NMI (normalized mutual
  * information) measurement based on the confusion matrix.
  * @author István Hegedűs
  *
  */
 public class NMI extends ConfusionMtxFunction {
-  
-  public static final double M_LOG2E = 1.4426950408889634074;
+	
 
-  @Override
-  public double postProcess(double meanError) {
-    double nmi = 0.0;
-    SparseVector expSum = new SparseVector();
-    SparseVector predSum = new SparseVector();
-    double N = 0.0;
-    double entropy = 0.0;
-    
-    int numClasses = 0;
-    SparseVector v;
-    for (int i : confusionMatrix.keySet()) {
-      v = confusionMatrix.get(i);
-      if (numClasses <= v.maxIndex()) {
-        numClasses = v.maxIndex() + 1;
-      }
-      if (numClasses <= i) {
-        numClasses = i + 1;
-      }
-      for (VectorEntry e : v) {
-        N += e.value;
-        expSum.put(i, expSum.get(i) + e.value);
-        predSum.put(e.index, predSum.get(e.index) + e.value);
-      }
-    }
-    
-    for (int i = 0; i < numClasses; i ++) {
-      double pi = expSum.get(i) / N;
-      double pj = predSum.get(i) / N;
-      v = confusionMatrix.get(i);
-      for (int j = 0; v != null && j < numClasses; j++) {
-        double value = v.get(j);
-        nmi += value == 0.0 ? 0.0 : (value / N) * Math.log((N * value) / (expSum.get(i) * predSum.get(j)))* M_LOG2E;
-      }
-      if (v != null) {
-        v.clear();
-      }
-      entropy -= (0.5 * pi * Math.log(pi) * M_LOG2E) + (0.5 * pj * Math.log(pj) * M_LOG2E);
-    }
-    return (N == 0.0 || Double.isNaN(entropy)) ? 0.0 : nmi / entropy;
-  }
+	public static final double M_LOG2E = 1.4426950408889634074;
+	
+	/***
+	 * Bugfixed by Arpad Berta
+	 */
+	@Override
+	public double postProcess(double meanError) {
+		double nmi = 0.0;
+		SparseVector expSum = new SparseVector();
+		SparseVector predSum = new SparseVector();
+		double N = 0.0;
+		double entropy = 0.0;
+
+		int numClasses = 0;
+		SparseVector v;
+		for (int i : confusionMatrix.keySet()) {
+			v = confusionMatrix.get(i);
+			if (numClasses <= v.maxIndex()) {
+				numClasses = v.maxIndex() + 1;
+			}
+			if (numClasses <= i) {
+				numClasses = i + 1;
+			}
+			for (VectorEntry e : v) {
+				N += e.value;
+				expSum.put(i, expSum.get(i) + e.value);
+				predSum.put(e.index, predSum.get(e.index) + e.value);
+			}
+		}
+
+		for (int i = 0; i < numClasses; i ++) {
+			if(N==0){
+				System.out.println(N);
+			}
+			double pi = expSum.get(i) / N;
+			double pj = predSum.get(i) / N;
+			v = confusionMatrix.get(i);
+			for (int j = 0; v != null && j < numClasses; j++) {
+				double value = v.get(j);
+				
+				double ep = expSum.get(i) * predSum.get(j);
+				double nvep = ep == 0.0 ? 0.0 : (N * value) / ep;
+				double lpij = nvep == 0.0 ? 0.0 : Math.log(nvep);
+				
+				nmi += value == 0.0  ? 0.0 : (value / N) * lpij * M_LOG2E;
+
+			}
+			if (v != null) {
+				v.clear();
+			}
+			double entropyi = pi == 0.0 ? 0.0 : pi * Math.log(pi) * M_LOG2E;
+		    double entropyj = pj == 0.0 ? 0.0 :pj * Math.log(pj) * M_LOG2E;
+			entropy -= 0.5 * (entropyi + entropyj);
+		}
+		return N == 0.0  ? 0.0 : nmi / entropy;
+	}
 
 }
