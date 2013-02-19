a16ff82bb164af4a78dc29762eaaa2739c025e24
Istvan Hegedus <ihegedus@inf.u-szeged.hu>
2013-02-19 13:54:18 +0100

    DFFT and DHWT were added to Utils.

--- src/gossipLearning/utils/Utils.java
+++ src/gossipLearning/utils/Utils.java
@@ -205,5 +205,143 @@ public class Utils {
     }
     return result;
   }
+  
+  /**
+   * Transforms the specified vector to the frequency space using Discrete 
+   * Fast Fourier Transformation (DFFT). The length of the specified vector should 
+   * be on power of 2. The vector contains complex numbers as pairs of real 
+   * and imaginary parts, so every even elements are the real parts of a complex 
+   * number and the corresponding imaginary part is the next one. The output 
+   * vector has the same structure.
+   * @param x to be transformed
+   * @return transformed vector
+   */
+  public static double[] dfft(double[] x) {
+    int N = x.length;
+
+    // base case
+    if (N == 2) return new double[]{x[0], x[1]};
+
+    // radix 2 Cooley-Tukey FFT
+    if (N % 2 != 0) { throw new RuntimeException("N is not a power of 2"); }
+
+    // fft of even terms
+    double[] even = new double[N/2];
+    for (int k = 0; k < N/4; k++) {
+        even[2*k] = x[4*k];
+        even[2*k + 1] = x[4*k + 1];
+    }
+    double[] q = dfft(even);
+
+    // fft of odd terms
+    double[] odd  = even;  // reuse the array
+    for (int k = 0; k < N/4; k++) {
+        odd[2*k] = x[4*k + 2];
+        odd[2*k + 1] = x[4*k + 3];
+    }
+    double[] r = dfft(odd);
+
+    // combine
+    double[] y = new double[N];
+    for (int k = 0; k < N/4; k++) {
+        double kth = -2 * k * Math.PI / (N/2);
+        double real = Math.cos(kth);
+        double imag = Math.sin(kth);
+        
+        y[2*k] = q[2*k] + (real*r[2*k] - imag*r[2*k + 1]);
+        y[2*k+1] = q[2*k + 1] + (imag*r[2*k] + real*r[2*k + 1]);
+        
+        y[2*k + N/2] = q[2*k] - (real*r[2*k] - imag*r[2*k + 1]);
+        y[2*k + N/2 + 1] = q[2*k + 1] - (imag*r[2*k] + real*r[2*k + 1]);
+    }
+    return y;
+  }
+  
+  /**
+   * Performs the Inverse Discrete Fast Fourier Transformation (iDFFT) on the   
+   * specified vector. The length of the specified vector should 
+   * be on power of 2. The vector contains complex numbers as pairs of real 
+   * and imaginary parts, so every even elements are the real parts of a complex 
+   * number and the corresponding imaginary part is the next one. The output 
+   * vector has the same structure.
+   * @param x to be transformed
+   * @return transformed vector
+   */
+  public static double[] idfft(double[] x) {
+    int N = x.length;
+    double[] y = new double[N];
+
+    // take conjugate
+    for (int i = 0; i < N/2; i++) {
+      y[2*i] = x[2*i];
+      y[2*i + 1] = -x[2*i + 1];
+    }
+
+    // compute forward FFT
+    y = dfft(y);
+
+    // take conjugate again
+    for (int i = 0; i < N/2; i++) {
+      y[2*i + 1] = -y[2*i + 1];
+    }
+
+    // divide by N
+    for (int i = 0; i < N; i++) {
+        y[i] = y[i] / (N/2);
+    }
+
+    return y;
+  }
+  
+  private static int waveLength = 2;
+  private static double[] coeffs = new double[]{1.0 / Math.sqrt(2.0), -1.0 / Math.sqrt(2.0)};
+  private static double[] scales = new double[]{-coeffs[1], coeffs[0]};
+  /**
+   * Performs the Discrete Haar Wavelet Transformation on the specified vector.
+   * The length of the vector should be even. 
+   * @param arrTime to be transformed
+   * @return transformed vector
+   */
+  public static double[] dhwt(double[] arrTime) {
+    double[] arrHilb = new double[arrTime.length];
+    int k = 0;
+    int h = arrTime.length >> 1;
+    for( int i = 0; i < h; i++ ) {
+      for( int j = 0; j < waveLength; j++ ) {
+        k = ( i << 1 ) + j;
+        while(k >= arrTime.length) {
+          k -= arrTime.length;
+        }
+        // low pass filter - energy (approximation)
+        arrHilb[ i ] += arrTime[ k ] * scales[ j ];
+        // high pass filter - details
+        arrHilb[ i + h ] += arrTime[ k ] * coeffs[ j ]; 
+      }
+    }
+    return arrHilb;
+  }
+  
+  /**
+   * Performs the Inverse Discrete Haar Wavelet Transformation on the specified vector.
+   * The length of the vector should be even. 
+   * @param arrTime to be transformed
+   * @return transformed vector
+   */
+  public static double[] idhwt(double[] arrHilb) {
+    double[] arrTime = new double[arrHilb.length];
+    int k = 0;
+    int h = arrHilb.length >> 1;
+    for( int i = 0; i < h; i++ ) {
+      for( int j = 0; j < waveLength; j++ ) {
+        k = ( i << 1 ) + j;
+        while(k >= arrHilb.length) {
+          k -= arrHilb.length;
+        }
+        // adding up details times energy (approximation)
+        arrTime[ k ] += ( arrHilb[ i ] * scales[ j ] + arrHilb[ i + h ] * coeffs[ j ] );
+      }
+    }
+    return arrTime;
+  }
 
 }
