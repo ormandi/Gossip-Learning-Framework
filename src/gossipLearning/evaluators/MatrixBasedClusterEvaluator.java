d5b01ba02ae1aec1bc11628938c45d4666b00728
Istvan Hegedus <ihegedus@inf.u-szeged.hu>
2013-05-24 15:52:28 +0200

    Cluster evaluator was debuged.

--- src/gossipLearning/evaluators/MatrixBasedClusterEvaluator.java
+++ src/gossipLearning/evaluators/MatrixBasedClusterEvaluator.java
@@ -1,5 +1,6 @@
 package gossipLearning.evaluators;
 
+import gossipLearning.interfaces.Evaluator;
 import gossipLearning.utils.SparseVector;
 import gossipLearning.utils.Utils;
 import gossipLearning.utils.VectorEntry;
@@ -27,15 +28,18 @@ import java.util.Vector;
  */
 public class MatrixBasedClusterEvaluator extends MatrixBasedEvaluator {
   private static final long serialVersionUID = -6354546808718608891L;
+  private double counter;
   
   public MatrixBasedClusterEvaluator() {
     values = new double[3];
     names = new String[]{"purity", "NMI", "RI"};
     mtx = new Vector<SparseVector>();
+    counter = 0.0;
   }
   
   public MatrixBasedClusterEvaluator(MatrixBasedClusterEvaluator a) {
     super(a);
+    counter = a.counter;
   }
   
   public Object clone() {
@@ -43,40 +47,49 @@ public class MatrixBasedClusterEvaluator extends MatrixBasedEvaluator {
   }
   
   @Override
-  public double[] getResults() {
+  public void merge(Evaluator evaluator) {
+    counter++;
+    MatrixBasedClusterEvaluator e = (MatrixBasedClusterEvaluator)evaluator;
     double putiry = 0.0, nmi = 0.0, sum = 0.0, max, normNmi = 0.0, ri = 0.0;
-    double[] sumCol = new double[mtx.size()];
-    double[] sumRow = new double[mtx.size()];
-    int[] matching = Utils.maximalMatching(mtx);
-    for (int i = 0; i < mtx.size(); i++) {
+    double[] sumCol = new double[e.mtx.size()];
+    double[] sumRow = new double[e.mtx.size()];
+    /*for (int i = 0; i < mtx.size(); i++) {
+      System.out.println(mtx.get(i));
+    }*/
+    int[] matching = Utils.maximalMatching(e.mtx);
+    //System.out.println(Arrays.toString(matching));
+    /*for (int i = 0; i < mtx.size(); i++) {
+      System.out.println(mtx.get(i));
+    }*/
+    for (int i = 0; i < e.mtx.size(); i++) {
       max = 0.0;
-      for (VectorEntry e : mtx.get(i)) {
-        sumRow[i] += e.value;
-        sumCol[e.index] += e.value;
-        sum += e.value;
-        if (e.value > max) {
-          max = e.value;
+      for (VectorEntry en : e.mtx.get(i)) {
+        sumRow[i] += en.value;
+        sumCol[en.index] += en.value;
+        sum += en.value;
+        if (en.value > max) {
+          max = en.value;
         }
-        if (matching[i] == e.index) {
-          ri += e.value;
+        if (matching[i] == en.index) {
+          ri += en.value;
         }
       }
       putiry += max;
     }
     if (sum == 0) {
-      return new double[] {0.0, 0.0, 0.0};
+      return;
     }
     putiry /= sum;
     ri /= sum;
-    for (int i = 0; i < mtx.size(); i++) {
+    for (int i = 0; i < e.mtx.size(); i++) {
       sumRow[i] /= sum;
       sumCol[i] /= sum;
     }
-    for (int i = 0; i < mtx.size(); i++) {
+    for (int i = 0; i < e.mtx.size(); i++) {
       double pi = sumRow[i];
-      for (int j = 0; j < mtx.size(); j++) {
+      for (int j = 0; j < e.mtx.size(); j++) {
         double pj = sumCol[j];
-        double value = mtx.get(i).get(j);
+        double value = e.mtx.get(i).get(j);
         double pij = (value / sum);
         nmi += (value == 0.0 || pi == 0.0 || pj == 0.0) ? 0.0 : pij * Math.log(pij / (pi * pj))* Utils.INVLN2;
       }
@@ -84,10 +97,24 @@ public class MatrixBasedClusterEvaluator extends MatrixBasedEvaluator {
       double entropyi = pi == 0.0 ? 0.0 : pi * Math.log(pi) * Utils.INVLN2;
       double entropyj = pj == 0.0 ? 0.0 : pj * Math.log(pj) * Utils.INVLN2;
       normNmi -= 0.5 * (entropyi + entropyj);
-      mtx.get(i).clear();
+      e.mtx.get(i).clear();
     }
     nmi /= normNmi;
-    return new double[] {putiry, nmi, ri};
+    values[0] += putiry;
+    values[1] += nmi;
+    values[2] += ri;
+  }
+  
+  @Override
+  public double[] getResults() {
+    double[] result = new double[values.length];
+    if (counter == 0.0) counter = 1.0;
+    for (int i = 0; i < values.length; i++) {
+      result[i] = values[i] / counter;
+      values[i] = 0.0;
+    }
+    counter = 0.0;
+    return result;
   }
   
   @Override
