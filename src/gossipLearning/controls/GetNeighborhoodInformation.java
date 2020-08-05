10078c7744730407f8a4cee1cda5ba80f1d8b84a
arppy <berta@inf.u-szeged.hu>
2020-08-05 12:44:35 +0200

    user trace assignments with node.id

--- src/gossipLearning/controls/GetNeighborhoodInformation.java
+++ src/gossipLearning/controls/GetNeighborhoodInformation.java
@@ -1,4 +1,13 @@
 package gossipLearning.controls;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 import peersim.config.Configuration;
 import peersim.config.FastConfig;
 import peersim.core.*;
@@ -6,16 +15,64 @@ import peersim.transport.Transport;
 
 public class GetNeighborhoodInformation implements Control {
   private static final String PROT = "protocol";
-
+  private static final String TRACE_FILE = "traceFile";
+  private final String fName;
+  protected LinkedList<String> userTracesSampler;
+  protected final LinkedList<String> userTraces;
+  protected Map<Long, String> assignedUserTrace;
   private final int pid;
 
   public GetNeighborhoodInformation(String prefix) {
     //System.err.println("HELLO "+PROT+" "+prefix);
     pid = Configuration.getPid(prefix + "." + PROT);
+    fName = Configuration.getString(prefix + "." + TRACE_FILE);
+    BufferedReader br;
+    userTraces = new LinkedList<String>();
+    userTracesSampler = new LinkedList<String>();
+    assignedUserTrace = new HashMap<Long, String>();
+	try {
+		br = new BufferedReader(new FileReader(fName));
+		String line;
+	    while ((line = br.readLine()) != null) {
+	      if (line != null) {
+	    	  userTraces.addLast(line);
+	    	  userTracesSampler.addLast(line);
+	      }
+	    }
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	if(userTraces.size()>0) {
+		Collections.shuffle(userTracesSampler, CommonState.r);
+		for (int i = 0; i < Network.size(); i ++) {
+	        Node node = Network.get(i);
+	        assignedUserTrace.put(node.getID(), userTracesSampler.poll());
+	        if (userTracesSampler.size() < 1) {
+	        	Collections.copy(userTracesSampler, userTraces);
+	        	Collections.shuffle(userTracesSampler, CommonState.r);
+	        }
+		}
+	}
+    
   }
 
   @Override
   public boolean execute() {
+    String outLine = "{";
+    long recentNodeId = Long.MIN_VALUE;
+	String recentAssignedUserTrace = "";
+	for (int i = 0; i < Network.size(); i++) {
+	  if (recentNodeId > Long.MIN_VALUE) {
+	    outLine += "\""+recentNodeId+"\": \""+recentAssignedUserTrace+"\", ";
+	  }
+      Node node = Network.get(i);
+      recentNodeId = node.getID();
+      recentAssignedUserTrace = assignedUserTrace.get(node.getID());
+    }
+	outLine += "\""+recentNodeId+"\": \""+recentAssignedUserTrace+"\"}";
+    System.out.println(outLine);    
     System.out.println("{");
     String nextOutLine = "";
     for (int i = 0; i < Network.size(); i++) {
